14ff954d57b3879b436a641ea012da0cefb31396
gOps132 <epilred@icloud.com>
2022-08-31 16:15:30 +0800

    added convenient vscode files

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,21 @@
 #include "vec3.h"
 #include "ray.h"
 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = r.get_origin() - center;
+    auto a = dot(r.get_direction(), r.get_direction());
+    auto b = 2.0 * dot(oc, r.get_direction());
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant > 0);
+}
+
 color ray_color(const ray& r) {
+    if (hit_sphere(point3(0,0,-1), 0.5, r))
+        return color(1, 0, 0);
     vec3 unit_direction = unit_vector(r.get_direction());
     auto t = 0.5*(unit_direction.y() + 1.0);
-    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+    return (1.0-t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
 int main(int argc, char** argv) 
